a1f5fdcbc4dc944d5ad7bbacb775cb9e6c92c966
codedubu <mccaine.river@gmail.com>
2021-01-19 08:46:22 -0800

    TakenDate Added

--- MedicationManager/MedicationManager/CoreData/TakenDate+Convenience.swift
+++ MedicationManager/MedicationManager/CoreData/TakenDate+Convenience.swift
@@ -5,4 +5,15 @@
 //  Created by River McCaine on 1/19/21.
 //
 
-import Foundation
+import CoreData
+
+extension TakenDate {
+    // Lets us create a TakenDate without capturing it
+    // NSManagedObjectContext is a platform where you manage your objects
+    @discardableResult convenience init(takenDate: Date, medication: Medication, context: NSManagedObjectContext = CoreDataStack.context ) {
+        // Allows the data to be saved in the right place.
+        self.init(context:context)
+        self.date = date
+        self.medication = medication
+    }
+}
